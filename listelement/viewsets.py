33bfb1c0917f595082cfcbb4b481c0e6bb3fb0f7
Mario Alonso <mario.blazter99@gmail.com>
2020-06-16 20:05:12 -0500

    8 Commit, Creando Metodos personalizados para el consumo de API

--- listelement/viewsets.py
+++ listelement/viewsets.py
@@ -1,6 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 from .models import Element, Category, Type
 from .serializer import ElementSerializer, CategorySerializer, TypeSerializer
+
+
 
 class ElementViewSet(viewsets.ModelViewSet):
     queryset = Element.objects.all()
@@ -9,6 +14,24 @@ class ElementViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    @action(detail=True, methods=['get'])
+    def elements(self, request, pk=None):
+        queryset = Element.objects.filter(category_id=pk)
+        serializer = ElementSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    '''def list(self, request):
+        queryset = Category.objects.all()
+        #print(queryset)
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Category.objects.all()
+        category = get_object_or_404(queryset, pk=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)'''
 
 class TypeViewSet(viewsets.ModelViewSet):
     queryset = Type.objects.all()
